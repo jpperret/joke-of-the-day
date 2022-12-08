d8dddf1abc6fa6d858a40740372c58ceb81c58eb
Jake Perret <djakeperret@gmail.com>
2022-12-08 18:39:35 -0500

    hardcoded date and joke

--- index.js
+++ index.js
@@ -18,26 +18,74 @@ app.get("/", (request, response) => {
   response.render("index");
 });
 
-app.get("/joke", (request, response) => {
+app.get("/joke", async (request, response) => {
   const name = request.query.name;
   var variables = {
     name: name,
-    joke: getJoke(name),
+    joke: await getJoke(name),
   };
   response.render("joke", variables);
 });
 
-app.listen(portNumber);
-console.log(`Web server started and running at http://localhost:${portNumber}`);
+var client;
+const databaseAndCollection = {
+  db: process.env.MONGO_DB_NAME,
+  collection: process.env.MONGO_COLLECTION,
+};
 
-function getJoke(name) {
+async function connectDB() {
+  const userName = process.env.MONGO_DB_USERNAME;
+  const password = process.env.MONGO_DB_PASSWORD;
+  const mongoURI = `mongodb+srv://${userName}:${password}@cluster0.sadwkuu.mongodb.net/?retryWrites=true&w=majority`;
+
+  client = new MongoClient(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+  try {
+    await client.connect();
+  } catch (e) {
+    console.error(e);
+    client.close();
+  }
+}
+
+async function getJoke(name) {
   // TODO get date
+  const date = "today";
 
   // TODO check if joke for name and date exists
+  let filter = { name: name, date: date };
+  const cursor = client
+    .db(databaseAndCollection.db)
+    .collection(databaseAndCollection.collection)
+    .find(filter);
+
+  result = await cursor.toArray();
 
   // TODO if no joke exists for name and date:
   // - Get a joke from humor api.
   //	- If daily request limit is hit then reuse a joke from a random name with a joke for today
   // - save joke in db (if reusing save under additional name)
-  return "{JOKE}";
+
+  if (result.length == 0) {
+    console.log("creating joke");
+    let newJoke = {
+      name: name,
+      date: date,
+      joke: "EXJOKE",
+    };
+    const result = await client
+      .db(databaseAndCollection.db)
+      .collection(databaseAndCollection.collection)
+      .insertOne(newJoke);
+    return await getJoke(name);
+  } else {
+    return result[0].joke;
+  }
 }
+
+app.listen(portNumber);
+connectDB();
+console.log(`Web server started and running at http://localhost:${portNumber}`);
